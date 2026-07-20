88eee2c31326a418f4b094a2ee769ff2bbb40f2d
Qareeb Sadi <karibsadi@gmail.com>
2026-07-20 08:14:10 -0700

    Connected Ghost HMS backend to PostgreSQL

---
+++ server/app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const pool = require("./config/db");
+
+const app = express();
+
+app.use(express.json());
+
+// Home Route
+app.get("/", (req, res) => {
+    res.send("🔥 THIS IS THE NEW GHOST HMS SERVER 🔥");
+});
+
+// Database Test
+app.get("/test-db", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.json(result.rows);
+    }catch (err) {
+    console.error("Database Error:");
+    console.error(err.message);
+
+    res.status(500).json({
+        success: false,
+        error: err.message
+    });
+}
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
